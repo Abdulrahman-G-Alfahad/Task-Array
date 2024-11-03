7ffdf14e8d5149e96c2ee7974236e76a0fe94f33
unknown <abdulrahman.g.alfahad@gmail.com>
2024-11-03 11:10:11 +0300

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        double[] temps = {21.2, 22.3, 26.4, 19.1, 34.7};
+
+        temps[2] = 48.9;
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println(temps[i]);
+        }
     }
 }
